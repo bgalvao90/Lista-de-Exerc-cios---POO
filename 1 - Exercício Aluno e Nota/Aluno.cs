37e7860e15170e864f8dca0db1e665f2df16a3b1
048783 <048783@UNI-LAB2-35>
2025-03-18 21:06:04 -0300

    Atividade

--- 1 - Exercício Aluno e Nota/Aluno.cs
+++ 1 - Exercício Aluno e Nota/Aluno.cs
@@ -18,8 +18,28 @@ namespace _1___Exercício_Aluno_e_Nota
         {
             NotaFinal = (NotaDaProva + NotaDoTrabalho) / 2;
         }
-        public void CalcularNotaFinal() 
+        public void CalcularNotaFinal()
         {
+            if (NotaFinal >= 7)
+            {
+                Console.WriteLine("Aprovado");
+            }
+            else
+            {
+                double notaRecuperacao = 12 - (NotaFinal / 2) ;
+                Console.WriteLine($"Reprovado! Será necessário de {notaRecuperacao} pontos ");
+            }
+        }
+        public void ImprimirNotaFinal()
+        {
+            Console.WriteLine($"Nota final: {NotaFinal}");
+        }
+        public void ReceberDados()
+        {
+            RA = double.Parse(Console.ReadLine());
+            Nome = Console.ReadLine();
+            NotaDaProva = double.Parse(Console.ReadLine());
+            NotaDoTrabalho = double.Parse(Console.ReadLine());
         }
     }
 }
--- 1 - Exercício Aluno e Nota/Program.cs
+++ 1 - Exercício Aluno e Nota/Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿using _1___Exercício_Aluno_e_Nota;
+
+Aluno aluno = new Aluno();
+
+Console.WriteLine("Digite o RA do aluno: ");
+aluno.RA = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o nome do aluno: ");
+aluno.Nome = Console.ReadLine();
+
+Console.WriteLine("Digite a nota da prova: ");
+aluno.NotaDaProva = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite a nota do trabalho: ");
+aluno.NotaDoTrabalho = double.Parse(Console.ReadLine());
+
+Console.Clear();
+Console.WriteLine($"RA: {aluno.RA}                     Nome do Aluno: {aluno.Nome}");
+
+aluno.CalcularMedia();
+
+aluno.ImprimirNotaFinal();
+
+aluno.CalcularNotaFinal();
+
+Console.ReadLine();
