35ec521312ed362631b00af30adf83abcd973c3d
GRUPPY02 <GRUPPY02@DESKTOP-B3K8HS6>
2025-03-20 10:13:48 -0300

    Atividades

---
+++ 4 - Exercício Criando um sistema de Lâmpada/Lampada.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4___Exercício_Criando_um_sistema_de_Lâmpada
+{
+    class Lampada
+    {
+        bool Ligada = false;
+
+        public void Ligar()
+        {
+            if (Ligada == false)
+            {
+                Ligada = true;
+            }
+        }
+        public void Desligar()
+        {
+            if (Ligada == true)
+            {
+                Ligada = false;
+            }
+        }
+        public void MostrarEstado()
+        {
+            if (Ligada == true)
+            {
+                Console.WriteLine("Lâmpada está ligada!");
+            }
+            else
+            {
+                Console.WriteLine("Lâmpada está desligada!");
+            }
+        }
+    }
+}
---
+++ 4 - Exercício Criando um sistema de Lâmpada/Program.cs
@@ -0,0 +1,41 @@
+﻿using _4___Exercício_Criando_um_sistema_de_Lâmpada;
+
+Lampada lampada = new Lampada();
+
+lampada.MostrarEstado();
+
+Console.WriteLine("Digite 1 se você deseja ligar a lâmpada, 2 para desligar ou 3 para mostrar o estado da lâmpada:");
+string opcao = Console.ReadLine();
+while (opcao != "4")
+{
+    if (opcao == "1")
+    {
+        lampada.Ligar();
+        Console.WriteLine();
+        Console.WriteLine("Digite 1 se você deseja ligar a lâmpada, 2 para desligar, 3 para mostrar o estado da lâmpada ou 4 para sair:");
+        opcao = Console.ReadLine();
+    }
+    else if (opcao == "2")
+    {
+        lampada.Desligar();
+        Console.WriteLine();
+        Console.WriteLine("Digite 1 se você deseja ligar a lâmpada, 2 para desligar, 3 para mostrar o estado da lâmpada ou 4 para sair:");
+        opcao = Console.ReadLine();
+    }
+    else if (opcao == "3")
+    {
+        lampada.MostrarEstado();
+        Console.WriteLine("Digite 1 se você deseja ligar a lâmpada, 2 para desligar, 3 para mostrar o estado da lâmpada ou 4 para sair:");
+        opcao = Console.ReadLine();
+    }
+    else if (opcao == "4")
+    {
+        lampada.MostrarEstado();
+    }
+    else
+    {
+        Console.WriteLine("Digite uma opção válida!");
+        Console.WriteLine("Digite 1 se você deseja ligar a lâmpada, 2 para desligar, 3 para mostrar o estado da lâmpada ou 4 para sair:");
+        opcao = Console.ReadLine();
+    }
+}
